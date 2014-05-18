d8b64ad5d9801cd99d1493b2b17a1b2c94a840ea
Alex Villa <avilla0429@gmail.com>
2014-05-18 18:17:31 -0400

    Changes app name to Template

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Golfymcg::Application.configure do
+Template::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
