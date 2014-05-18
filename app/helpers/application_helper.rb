2a78c6b9a9f6b960de02618c67702ffc7288e887
Alex Villa <avilla0429@gmail.com>
2014-05-18 17:02:10 -0400

    Adds page specific versus global javascript importing Adjusts header styling including removing the search bar Moves nav links into a helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,28 @@ module ApplicationHelper
   def print_page_namespace
     "page-" + params[:controller] + " " + "page-" + params[:controller] + "-" + params[:action]
   end
+
+  def javascript_action_sub(action_name)
+    new_action = action_name.sub 'create', 'new'
+    new_action.sub! 'update', 'edit'
+    return new_action
+  end
+
+  def javascript_include_page_tag
+    tag_name = params[:controller]+"_"+javascript_action_sub(params[:action])
+    asset_location = Rails.application.assets.find_asset tag_name
+    unless asset_location.nil?
+      javascript_include_tag tag_name
+    end
+  end
+
+  def nav_link(name)
+      content_tag(
+        :li,
+        content_tag(
+          :a,
+          name,
+          href: "javascript:void(0)"),
+        class: "nav-link")
+  end
 end
